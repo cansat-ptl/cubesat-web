0f2ccf8d74a0c51fd4d36b285085c243b8bd5a61
artshellorok <artoncobaka@list.ru>
2020-03-27 03:46:06 +0900

    linting + css fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,67 @@
-let gulp = require('gulp');
-    posthtml = require('gulp-posthtml');
-    inline = require('gulp-inline-source');
-    sass = require('gulp-sass');
-    del = require('del');
-    htmlmin = require('gulp-htmlmin');
-    connect = require('gulp-connect');
+let gulp = require("gulp");
+posthtml = require("gulp-posthtml");
+inline = require("gulp-inline-source");
+sass = require("gulp-sass");
+del = require("del");
+htmlmin = require("gulp-htmlmin");
+connect = require("gulp-connect");
 
 let minify_config = {
   collapseWhitespace: true,
   minifyJS: true,
   minifyCSS: true
-}
+};
 
-gulp.task('sass', () => {
-  return gulp.src('src/css/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'));
+gulp.task("sass", () => {
+  return gulp
+    .src("src/css/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("dist/css"));
 });
 
-gulp.task('js', () => {
-  return gulp.src('src/js/*.js')
-    .pipe(gulp.dest('dist/js'));
+gulp.task("js", () => {
+  return gulp.src("src/js/*.js").pipe(gulp.dest("dist/js"));
 });
 
-gulp.task('icons', () => {
-  return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
-      .pipe(gulp.dest('dist/webfonts/'));
+gulp.task("icons", () => {
+  return gulp
+    .src("node_modules/@fortawesome/fontawesome-free/webfonts/*")
+    .pipe(gulp.dest("dist/webfonts/"));
 });
 
-gulp.task('img', () => {
-  return gulp.src('src/images/*')
-    .pipe(gulp.dest('dist/images'));
-})
+gulp.task("img", () => {
+  return gulp.src("src/images/*").pipe(gulp.dest("dist/images"));
+});
 
-gulp.task('html', () => {
-  return gulp.src('src/pages/*.html')
+gulp.task("html", () => {
+  return gulp
+    .src("src/pages/*.html")
     .pipe(posthtml())
-    .pipe(gulp.dest('dist'));
-})
+    .pipe(gulp.dest("dist"));
+});
 
-gulp.task('minimize', () => {
-  return gulp.src('dist/*.html')
+gulp.task("minimize", () => {
+  return gulp
+    .src("dist/*.html")
     .pipe(inline())
     .pipe(htmlmin(minify_config))
-    .pipe(gulp.dest('dist'));
-})
-
-gulp.task('clean', () => {
-     return del(['dist/css', 'dist/js'], {force:true});
+    .pipe(gulp.dest("dist"));
 });
 
-gulp.task('default', gulp.series('sass', 'js', 'icons', 'img', 'html', 'minimize', 'clean'));
+gulp.task("clean", () => {
+  return del(["dist/css", "dist/js"], { force: true });
+});
 
+gulp.task(
+  "default",
+  gulp.series("sass", "js", "icons", "img", "html", "minimize", "clean")
+);
 
-gulp.task('serve', async function() {
+gulp.task("serve", async function() {
   connect.server({
-    root: './dist',
+    root: "./dist",
     port: 8080,
     livereload: true
   });
-  gulp.watch('./src/*.html', gulp.series('default'));
+  gulp.watch("./src/**/*", gulp.series("default"));
 });
